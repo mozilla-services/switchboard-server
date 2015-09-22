76a9c0da522694ab8e1f6a82c6fe23603a786115
Miles Crabill <mcrabill@mozilla.com>
2015-09-22 11:30:57 -0700

    add some comments, make sure that loading config from env works

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ var app = express();
 app.get('/', function(req, res) {
     // manager constructor takes query args and a # of buckets
     let manager = new Manager(req.query, config.get('num_buckets'));
-    let experiment = new Experiment(manager);
     let results = {
         'onboarding-a': manager.turnOnBucket(0, 50),
         'onboarding-b': manager.turnOnBucket(50, 100)
     }
+
+    // return experiment results
+    // let experiment = new Experiment(manager);
     // let results = experiments.sample();
+
     res.send(JSON.stringify(results));
 })
 
@@ -26,4 +29,4 @@ app.get('/' + config.get('update_server_url'), function(req, res) {
     res.send('update');
 })
 
-app.listen(8080)
+app.listen(8080);
