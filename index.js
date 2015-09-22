8c899906fa61c43b0086dc770f22973ff8c72f78
Miles Crabill <mcrabill@mozilla.com>
2015-09-21 19:56:57 -0700

    shuffle around and rename things to be more nodey

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 import express from 'express';
 
 // local imports
-import config from './config.js';
-import Manager from './manager.js';
-import Experiments from './experiments.js';
+import config from './lib/config.js';
+import Manager from './lib/manager.js';
+import Experiment from './lib/experiment.js';
 
 var app = express();
 
+// main route
 app.get('/', function(req, res) {
+    // manager constructor takes query args and a # of buckets
     let manager = new Manager(req.query, config.get('num_buckets'));
-    let experiments = new Experiments(manager);
+    let experiment = new Experiment(manager);
     let results = {
         'onboarding-a': manager.turnOnBucket(0, 50),
         'onboarding-b': manager.turnOnBucket(50, 100)
@@ -19,6 +21,7 @@ app.get('/', function(req, res) {
     res.send(JSON.stringify(results));
 })
 
+// update route
 app.get('/' + config.get('update_server_url'), function(req, res) {
     res.send('update');
 })
