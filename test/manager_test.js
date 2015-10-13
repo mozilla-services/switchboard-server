6c9b084b93a4e0c217e01a44140e6ed67a5ad868
Miles Crabill <mcrabill@mozilla.com>
2015-10-13 15:51:06 -0700

    add and fix tests

--- test/manager_test.js
+++ test/manager_test.js
@@ -13,24 +13,24 @@ describe('Manager', function() {
         });
         it('should work for negatives', function() {
             manager.uuid = -3;
-            manager.getUserBucket().should.equal(-82);
+            manager.getUserBucket().should.equal(14);
         });
         it('should work for strings', function() {
             manager.uuid = 'foo bar qux joke string';
-            manager.getUserBucket().should.equal(-49);
+            manager.getUserBucket().should.equal(47);
         });
     });
     describe('#isInBucket(low, high)', function() {
         let manager = new Manager({'lang': 'eng'}, numBuckets);
-        it('should return false if low <= userBucket < high', function() {
+        it('should return true if low <= userBucket < high', function() {
             manager.uuid = 50;
             let bucket = manager.getUserBucket(this.uuid);
-            manager.isInBucket(bucket, bucket + 1).should.be.false();
+            manager.isInBucket(bucket, bucket + 1).should.be.true();
         })
-        it('should return true if low == high', function() {
+        it('should return false if low == high', function() {
             manager.uuid = 50;
             let bucket = manager.getUserBucket(this.uuid);
-            manager.isInBucket(bucket, bucket).should.be.true();
+            manager.isInBucket(bucket, bucket).should.be.false();
         })
     })
     describe('#isApplicationId()', function() {
--- test/server_test.js
+++ test/server_test.js
@@ -5,16 +5,21 @@ import * as should from 'should';
 import app from '../lib/app.js';
 import config from '../lib/config.js';
 
+// example experiments to test
+// gets dirname (test), goes up a dir to config
+config.set('experiments_file', __dirname + '/../config/experiments.json')
+
 // fetch all this from config
 const ip = config.get('ip');
 const port = config.get('port');
 const main_server_url = config.get('main_server_url');
-const urls_url = config.get('urls_url');
+const base_url = config.get('base_url');
+const urls_url = base_url + config.get('urls_url');
 
-const v2_url = main_server_url + 'v2';
-const v1_url = main_server_url + 'v1';
+const v2_url = base_url + 'v2';
+const v1_url = base_url + 'v1';
 
-describe('GET / with lang = eng', function() {
+describe('GET /v1 with lang = eng and uuid = foo', function() {
     it('should respond with JSON', function(done) {
         request(app)
             .get(v1_url + '/?lang=eng')
@@ -22,16 +27,37 @@ describe('GET / with lang = eng', function() {
             .expect('Content-Type', /json/)
             .expect(200, done);
     })
-    it('if the app is active, it should respond as active', function(done) {
+    it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
         request(app)
-            .get(v1_url + '/?lang=eng?uuid=foo')
+            .get(v1_url + '/?lang=eng&uuid=foo')
             .set('Accept', 'application/json')
             .expect(function(res) {
-                let jsonResponse = JSON.parse(res.body);
-                console.log(jsonResponse);
-                // if (jsonResponse['onboarding-a'].isActive != true) {
-                //     throw new Error('onboarding-a should be ' + true + ', reported ' + jsonResponse['onboarding-a'].isActive)
-                // }
+                if (res.body['onboarding-a'].isActive != true &&
+                    res.body['onboarding-b'].isActive != true) {
+                        throw new Error('one of onboarding-a or onboarding-b should be true')
+                }
+            })
+            .expect(200, done);
+    })
+})
+
+describe('GET /v2 with lang = eng and uuid = foo', function() {
+    it('should respond with JSON', function(done) {
+        request(app)
+            .get(v2_url + '/?uuid=foo')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    })
+    it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
+        request(app)
+            .get(v2_url + '/?lang=eng&uuid=foo')
+            .set('Accept', 'application/json')
+            .expect(function(res) {
+                if (res.body['results']['onboarding-a'].isActive != true &&
+                    res.body['results']['onboarding-b'].isActive != true) {
+                        throw new Error('one of onboarding-a or onboarding-b should be true')
+                }
             })
             .expect(200, done);
     })
