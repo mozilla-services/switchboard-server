87f065da84328f25df4dfa0d4ac6536e9ba46185
Miles Crabill <mcrabill@mozilla.com>
2015-12-30 14:00:51 -0800

    fix indentation

--- test/server_test.js
+++ test/server_test.js
@@ -22,45 +22,45 @@ const v2_url = baseUrl + 'v2';
 const v1_url = baseUrl + 'v1';
 
 describe('GET /v1 with lang = eng and uuid = foo', function() {
-    it('should respond with JSON', function(done) {
-        request(app)
-            .get(v1_url + '/?lang=eng')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    })
-    it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
-        request(app)
-            .get(v1_url + '/?lang=eng&uuid=foo')
-            .set('Accept', 'application/json')
-            .expect(function(res) {
-                if (res.body['onboarding-a'].isActive != true &&
-                    res.body['onboarding-b'].isActive != true) {
-                        throw new Error('one of onboarding-a or onboarding-b should be true')
-                }
-            })
-            .expect(200, done);
-    })
+  it('should respond with JSON', function(done) {
+    request(app)
+      .get(v1_url + '/?lang=eng')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+  it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
+    request(app)
+      .get(v1_url + '/?lang=eng&uuid=foo')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        if (res.body['onboarding-a'].isActive != true &&
+          res.body['onboarding-b'].isActive != true) {
+          throw new Error('one of onboarding-a or onboarding-b should be true')
+        }
+      })
+      .expect(200, done);
+  })
 })
 
 describe('GET /v2 with lang = eng and uuid = foo', function() {
-    it('should respond with JSON', function(done) {
-        request(app)
-            .get(v2_url + '/?uuid=foo')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    })
-    it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
-        request(app)
-            .get(v2_url + '/?lang=eng&uuid=foo')
-            .set('Accept', 'application/json')
-            .expect(function(res) {
-                if (res.body['results']['onboarding-a'].isActive != true &&
-                    res.body['results']['onboarding-b'].isActive != true) {
-                        throw new Error('one of onboarding-a or onboarding-b should be true')
-                }
-            })
-            .expect(200, done);
-    })
+  it('should respond with JSON', function(done) {
+    request(app)
+      .get(v2_url + '/?uuid=foo')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+  it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
+    request(app)
+      .get(v2_url + '/?lang=eng&uuid=foo')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        if (res.body['results']['onboarding-a'].isActive != true &&
+          res.body['results']['onboarding-b'].isActive != true) {
+          throw new Error('one of onboarding-a or onboarding-b should be true')
+        }
+      })
+      .expect(200, done);
+  })
 })
