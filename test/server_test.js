8c436d5f8aa5ce7e473455d659076b0381ad18de
Miles Crabill <mcrabill@mozilla.com>
2016-02-25 17:00:31 -0800

    test appId matches using regexp string delimiters

--- test/server_test.js
+++ test/server_test.js
@@ -34,8 +34,8 @@ describe('GET /v1 with lang = eng and uuid = foo', function() {
       .get(v1Url + '/?lang=eng&uuid=foo')
       .set('Accept', 'application/json')
       .expect(function(res) {
-        if (res.body['onboarding-a'].isActive != true &&
-          res.body['onboarding-b'].isActive != true) {
+        if (res.body['onboarding-a'].isActive !== true &&
+          res.body['onboarding-b'].isActive !== true) {
           throw new Error('one of onboarding-a or onboarding-b should be true')
         }
       })
@@ -56,9 +56,45 @@ describe('GET /v2 with lang = eng and uuid = foo', function() {
       .get(v2Url + '/?lang=eng&uuid=foo')
       .set('Accept', 'application/json')
       .expect(function(res) {
-        if (res.body['results']['onboarding-a'].isActive != true &&
-          res.body['results']['onboarding-b'].isActive != true) {
+        if (res.body['results']['onboarding-a'].isActive !== true &&
+          res.body['results']['onboarding-b'].isActive !== true) {
           throw new Error('one of onboarding-a or onboarding-b should be true')
+        }
+      })
+      .expect(200, done);
+  })
+})
+
+describe('GETs to /v1', function() {
+  it('should respond with JSON', function(done) {
+    request(app)
+      .get(v2Url + '/?uuid=foo')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+  it('if uuid is given, one of onboarding-a or onboarding-b should be active', function(done) {
+    request(app)
+      .get(v2Url + '/?lang=eng&uuid=foo')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        if (res.body['results']['onboarding-a'].isActive !== true &&
+          res.body['results']['onboarding-b'].isActive !== true) {
+          throw new Error('one of onboarding-a or onboarding-b should be true')
+        }
+      })
+      .expect(200, done);
+  })
+  it('should require exact string matches for appId no regex syntax is used', function(done) {
+    request(app)
+      .get(v1Url + '/?uuid=test&appId=org.mozilla.fennec_aurora')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        if (res.body['fennec_aurora'].isActive !== true) {
+          throw new Error('fennec_aurora is not active and should be')
+        }
+        if (res.body['fennec'].isActive === true) {
+          throw new Error('fennec is active and should not be')
         }
       })
       .expect(200, done);
